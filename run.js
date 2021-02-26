962ebedbafc949dd35cd08c6b198fa6218c3056c
Thomas Backlund <bashlund@universe.ai>
2021-02-26 12:20:10 +0100

    Add debugging function to run.js for browser friendliness

--- run.js
+++ run.js
@@ -14,7 +14,24 @@ function run()
     const filePath = process.argv[2] || "./user1.json";
     const userConfig = {};
 
-    const messagingApp = new MessagingApp(filePath, userConfig);
+    // DEBUG: Set this to true to load config into memory already here.
+    // This is to see that Config can work as in a browser environment.
+    const testAsIfBrowser = false;
+
+    let objectOrFilePath;
+
+    if (testAsIfBrowser) {
+        const fs = require("fs");
+        objectOrFilePath = JSON.parse(fs.readFileSync(filePath));
+        if (typeof objectOrFilePath.app.keyPair === "string") {
+            objectOrFilePath.app.keyPair = JSON.parse(fs.readFileSync(objectOrFilePath.app.keyPair));
+        }
+    }
+    else {
+        objectOrFilePath = filePath;
+    }
+
+    const messagingApp = new MessagingApp(objectOrFilePath, userConfig);
     messagingApp.init();
 }
 
